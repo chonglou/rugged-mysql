8c4223603fb869ceb77e185b652c042b7d9d97c2
flamen <jitang.zheng@gmail.com>
2014-11-18 20:03:36 -0800

    indent src file

--- ext/rugged/mysql/rugged_mysql.c
+++ ext/rugged/mysql/rugged_mysql.c
@@ -4,10 +4,11 @@ VALUE rb_mRugged;
 VALUE rb_mRuggedMysql;
 VALUE rb_cRuggedBackend;
 
-
-void Init_rugged_mysql(void) {
-    rb_mRugged = rb_const_get(rb_cObject, rb_intern("Rugged"));
-    rb_cRuggedBackend = rb_const_get(rb_mRugged, rb_intern("Backend"));
-    rb_mRuggedMysql = rb_const_get(rb_mRugged, rb_intern("Mysql"));
-    Init_rugged_mysql_backend();
+void
+Init_rugged_mysql (void)
+{
+  rb_mRugged = rb_const_get (rb_cObject, rb_intern ("Rugged"));
+  rb_cRuggedBackend = rb_const_get (rb_mRugged, rb_intern ("Backend"));
+  rb_mRuggedMysql = rb_const_get (rb_mRugged, rb_intern ("Mysql"));
+  Init_rugged_mysql_backend ();
 }
--- ext/rugged/mysql/rugged_mysql_backend.c
+++ ext/rugged/mysql/rugged_mysql_backend.c
@@ -10,60 +10,77 @@ extern VALUE rb_cRuggedBackend;
 
 VALUE rb_cRuggedMysqlBackend;
 
-int git_odb__error_ambiguous(char*);
-int git_odb__error_notfound(char*);
-int git_odb__error_ambiguous(char*);
+int git_odb__error_ambiguous (char *);
+int git_odb__error_notfound (char *);
+int git_odb__error_ambiguous (char *);
 
-typedef struct _rugged_backend {
-  int (* odb_backend)(git_odb_backend **backend_out, struct _rugged_backend *backend, const char* path);
-  int (* refdb_backend)(git_refdb_backend **backend_out, struct _rugged_backend *backend, const char* path);
+typedef struct _rugged_backend
+{
+  int (*odb_backend) (git_odb_backend ** backend_out,
+		      struct _rugged_backend * backend, const char *path);
+  int (*refdb_backend) (git_refdb_backend ** backend_out,
+			struct _rugged_backend * backend, const char *path);
 } rugged_backend;
 
-
-typedef struct {
-    rugged_backend backend;
-    char *host;
-    int port;
-    char *socket;
-    char *username;
-    char *password;
-    char *database;
+typedef struct
+{
+  rugged_backend backend;
+  char *host;
+  int port;
+  char *socket;
+  char *username;
+  char *password;
+  char *database;
 } rugged_mysql_backend;
 
-static void rb_rugged_mysql_backend__free(rugged_mysql_backend *backend) {
-    free(backend->host);
-    free(backend->socket);
-    free(backend->username);
-    if(backend->password != NULL){
-        free(backend->password);
+static void
+rb_rugged_mysql_backend__free (rugged_mysql_backend * backend)
+{
+  free (backend->host);
+  free (backend->socket);
+  free (backend->username);
+  if (backend->password != NULL)
+    {
+      free (backend->password);
     }
-    free(backend->database);
-    free(backend);
+  free (backend->database);
+  free (backend);
 }
 
-static int rugged_mysql_odb_backend(git_odb_backend** backend_out, rugged_backend* backend, const char* path){
-    //todo
-    return -1;
+static int
+rugged_mysql_odb_backend (git_odb_backend ** backend_out,
+			  rugged_backend * backend, const char *path)
+{
+  //todo
+  return -1;
 }
 
-static int rugged_mysql_refdb_backend(git_refdb_backend** backend_out, rugged_backend* backend, const char* path){
-    //todo
-    return -1;
+static int
+rugged_mysql_refdb_backend (git_refdb_backend ** backend_out,
+			    rugged_backend * backend, const char *path)
+{
+  //todo
+  return -1;
 }
 
-static rugged_mysql_backend *rugged_mysql_backend_new(char* host, int port, char* socket, char* username, char* password, char* database) {
-    rugged_mysql_backend* mysql_backend = malloc(sizeof(rugged_mysql_backend));
+static rugged_mysql_backend *
+rugged_mysql_backend_new (char *host, int port,
+			  char *socket,
+			  char *username, char *password, char *database)
+{
+  rugged_mysql_backend *mysql_backend =
+    malloc (sizeof (rugged_mysql_backend));
 
-    mysql_backend->backend.odb_backend = rugged_mysql_odb_backend;
-    mysql_backend->backend.refdb_backend = rugged_mysql_refdb_backend;
+  mysql_backend->backend.odb_backend = rugged_mysql_odb_backend;
+  mysql_backend->backend.refdb_backend = rugged_mysql_refdb_backend;
 
-    mysql_backend->host = strdup(host);
-    mysql_backend->port = port;
-    mysql_backend->socket = strdup(socket);
-    mysql_backend->username = strdup(username);
-    mysql_backend->password = password == NULL ? NULL : strdup(password);
+  mysql_backend->host = strdup (host);
+  mysql_backend->port = port;
+  mysql_backend->socket = strdup (socket);
+  mysql_backend->username = strdup (username);
+  mysql_backend->password = password == NULL ? NULL : strdup (password);
 
-    return mysql_backend;
+  return mysql_backend;
 }
 
 /*
@@ -76,54 +93,64 @@ opts - hash containing the connection options.
 :username - (optional) string, default root
 :database - string
 */
-static VALUE rb_rugged_mysql_backend_new(VALUE klass, VALUE rb_opts)
+static VALUE
+rb_rugged_mysql_backend_new (VALUE klass, VALUE rb_opts)
 {
-    VALUE val;
-    char *host;
-    char *socket="/var/run/mysqld/mysqld.sock";
-    char *database;
-    char *username = "root";
-    char *password = NULL;
-    int port=3306;
+  VALUE val;
+  char *host;
+  char *socket = "/var/run/mysqld/mysqld.sock";
+  char *database;
+  char *username = "root";
+  char *password = NULL;
+  int port = 3306;
 
-    Check_Type(rb_opts, T_HASH);
+  Check_Type (rb_opts, T_HASH);
 
-    if ((val = rb_hash_aref(rb_opts, ID2SYM(rb_intern("host")))) != Qnil) {
-        Check_Type(val, T_STRING);
-        host = StringValueCStr(val);
+  if ((val = rb_hash_aref (rb_opts, ID2SYM (rb_intern ("host")))) != Qnil)
+    {
+      Check_Type (val, T_STRING);
+      host = StringValueCStr (val);
     }
 
-    if ((val = rb_hash_aref(rb_opts, ID2SYM(rb_intern("port")))) != Qnil) {
-        Check_Type(val, T_FIXNUM);
-        port = NUM2INT(val);
+  if ((val = rb_hash_aref (rb_opts, ID2SYM (rb_intern ("port")))) != Qnil)
+    {
+      Check_Type (val, T_FIXNUM);
+      port = NUM2INT (val);
     }
 
-    if ((val = rb_hash_aref(rb_opts, ID2SYM(rb_intern("socket")))) != Qnil) {
-        Check_Type(val, T_STRING);
-        socket = StringValueCStr(val);
+  if ((val = rb_hash_aref (rb_opts, ID2SYM (rb_intern ("socket")))) != Qnil)
+    {
+      Check_Type (val, T_STRING);
+      socket = StringValueCStr (val);
     }
 
-    if ((val = rb_hash_aref(rb_opts, ID2SYM(rb_intern("username")))) != Qnil) {
-        Check_Type(val, T_STRING);
-        username = StringValueCStr(val);
+  if ((val = rb_hash_aref (rb_opts, ID2SYM (rb_intern ("username")))) != Qnil)
+    {
+      Check_Type (val, T_STRING);
+      username = StringValueCStr (val);
     }
 
-
-    if ((val = rb_hash_aref(rb_opts, ID2SYM(rb_intern("password")))) != Qnil) {
-        Check_Type(val, T_STRING);
-        password = StringValueCStr(val);
+  if ((val = rb_hash_aref (rb_opts, ID2SYM (rb_intern ("password")))) != Qnil)
+    {
+      Check_Type (val, T_STRING);
+      password = StringValueCStr (val);
     }
 
+  val = rb_hash_aref (rb_opts, ID2SYM (rb_intern ("database")));
+  Check_Type (val, T_STRING);
+  database = StringValueCStr (val);
 
-    val = rb_hash_aref(rb_opts, ID2SYM(rb_intern("database")));
-    Check_Type(val, T_STRING);
-    database = StringValueCStr(val);
-
-    return Data_Wrap_Struct(klass, NULL, rb_rugged_mysql_backend__free, rugged_mysql_backend_new(host, port, socket, username, password, database));
+  return Data_Wrap_Struct (klass, NULL, rb_rugged_mysql_backend__free,
+			   rugged_mysql_backend_new (host, port, socket,
+						     username, password,
+						     database));
 }
 
-
-void Init_rugged_mysql_backend(void) {
-    rb_cRuggedMysqlBackend = rb_define_class_under(rb_mRuggedMysql, "Backend", rb_cRuggedBackend);
-    rb_define_singleton_method(rb_cRuggedMysqlBackend, "new", rb_rugged_mysql_backend_new, 1);
+void
+Init_rugged_mysql_backend (void)
+{
+  rb_cRuggedMysqlBackend =
+    rb_define_class_under (rb_mRuggedMysql, "Backend", rb_cRuggedBackend);
+  rb_define_singleton_method (rb_cRuggedMysqlBackend, "new",
+			      rb_rugged_mysql_backend_new, 1);
 }
